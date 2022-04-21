c617f3f26b45e61587180773f2dcb8fd82039d28
Krishna Rijal <hope143701@gmail.com>
2022-04-21 17:00:02 +0545

    admin initialted and initial file

---
+++ src/main/java/com/practice/services/AdminServices.java
@@ -0,0 +1,10 @@
+package com.practice.services;
+
+public class AdminServices {
+    public boolean getLoginResponse(String id, String username)
+    {
+        System.out.println(username + " " + id);
+        return username.equals("Krishna Rijal") && id.equals("hello123");
+    }
+
+}
